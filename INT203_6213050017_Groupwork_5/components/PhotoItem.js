79e880fa69bbe5b357ef8c84d8ce04afdf980b99
Jirawath <Jirawath.jiw@mail.kmutt.ac.th>
2021-02-26 16:10:26 +0700

    fix photo_resolution

--- INT203_6213050017_Groupwork_5/components/PhotoItem.js
+++ INT203_6213050017_Groupwork_5/components/PhotoItem.js
@@ -13,7 +13,7 @@ app.component('photo-item', {
     `
     <div class="p-10 inline-flex space-x-10 w-full">
         <div v-show="pic.isPhotoitem" v-for="(pic,index) in gallery">
-        <img v-bind:src="pic.src" v-on:click="toggleZoom(index)" class="w-80 h-52 cursor-pointer">
+        <img v-bind:src="pic.src" v-on:click="toggleZoom(index)" class="w-100% h-auto cursor-pointer">
         <div class="inline-flex w-full">
             <div class="w-1/2 text-gray-300 py-2 text-base">{{pic.picture_name}}</div>
             <div class="text-right w-1/2 py-2">
